6886dfe028a8fd4157c3168fb38dfb1cf9b8409d
tokunami <nami.tokunaga@fujixerox.co.jp>
2018-04-26 23:29:20 -0700

    fix typo

--- app.py
+++ app.py
@@ -40,7 +40,6 @@ app = Flask(__name__)
 
 @app.route("/")
 def welcome():
-    """List all available api routes."""
     return (
         f"Available Routes:<br/>"
         f"/api/v1.0/preceipitation<br/>"
@@ -70,22 +69,32 @@ def stations():
     # Return a json list of stations from the dataset.
     # Query
     results = session.query(Sta.station, Sta.name).all()
-    
-    all_stas = list(np.ravel(results))
+    all_stas = []
+    for result in results:
+        result_dict = {}
+        result_dict['station'] = result.station
+        result_dict['name'] = result.name
+        all_stas.append(result_dict)
 
     return jsonify(all_stas)
 
 
 @app.route("/api/v1.0/tobs")
-def tobs():
-    results = session.query(Mea.tobs).filter(Mea.date > '2016-12-31', Mea.date < '2018-01-01').all()
-    all_stas = list(np.ravel(results))
-    return jsonify(all_stas)
+def tobss():
+    results = session.query(Mea.date, Mea.tobs).filter(Mea.date > '2016-12-31', Mea.date < '2018-01-01').all()
+    all_tobs = []
+    for result in results:
+        result_dict = {}
+        result_dict['date'] = result.date
+        result_dict['tobs'] = result.tobs
+        all_tobs.append(result_dict)
 
+    return jsonify(all_tobs)
 
 @app.route("/api/v1.0/<start>")
 @app.route("/api/v1.0/<start>/<end>")
-def descr(start, end=session.query(Mea.date).order_by(Mea.date.desc()).first()[0]):
+def descr(start, end=None):
+    if end == None: end = session.query(Mea.date).order_by(Mea.date.desc()).first()[0]
     tobs = pd.read_sql(session.query(Mea.tobs).filter(Mea.date > start, Mea.date <= end).statement, session.bind)
     
     tobs_dict = {}
